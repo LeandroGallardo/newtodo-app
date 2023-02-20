f1298def8b9f5dd286f74d2adf15d57f7dbfed98
Leandro Gallardo <l.gallardo.fernandez00@gmail.com>
2023-02-19 21:55:51 -0300

    prueba de git

--- src/components/task.jsx
+++ src/components/task.jsx
@@ -13,7 +13,7 @@ const Task = ({ Tarea, changeTask,setisEdit }) => {
                 </p>
                 <button type="button" onClick={()=>handleclick(Tarea.id)}
                     className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
-                    editar tarea
+                    Modificar Tarea
                 </button>
             </div>
         </div>
